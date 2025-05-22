ea84b43f5bfdc11124686d36eff839f7ed13fc81
DG-Anef <plunghi@anef-provence.com>
2025-05-22 22:53:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base:"/aemo13/",
     plugins: [plugin()],
     server: {
         host:'localhost',
