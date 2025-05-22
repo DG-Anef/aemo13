c39bd86eabaea298ea5d244d1bc4c05de02a709d
DG-Anef <plunghi@anef-provence.com>
2025-05-23 00:12:30 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,8 +21,8 @@ export default function Login({ supabase, profile }) {
             console.error("Erreur de connexion :", authError.message);
             setError(authError.message);
         } else {
-            navigate("/aemo13/dashboard");
-            window.location.href = "/aemo13/";
+            navigate("/dashboard");
+            window.location.href = "/index.html";
         }
     }
 
